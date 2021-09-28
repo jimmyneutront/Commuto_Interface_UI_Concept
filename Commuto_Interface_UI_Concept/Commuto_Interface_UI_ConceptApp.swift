d623ff3740feed4e6f0c4e15dc1eac128bdf3c6c
jimmyneutront <nubxic-5meVbe-nujcuh>
2021-09-28 09:21:35 -0400

    Initial Commit

---
+++ Commuto_Interface_UI_Concept/Commuto_Interface_UI_ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Commuto_Interface_UI_ConceptApp.swift
+//  Commuto_Interface_UI_Concept
+//
+//  Created by James Telzrow on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Commuto_Interface_UI_ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Commuto_Interface_UI_Concept/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Commuto_Interface_UI_Concept
+//
+//  Created by James Telzrow on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
